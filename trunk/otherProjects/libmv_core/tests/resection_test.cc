ef652abcdbcbbb041e1426bbda8061946db1bda7
thibault.lelore@gmail.com <thibault.lelore@gmail.com@925df00f-a3a1-6e17-06e6-3e40b9fa844e>
2011-06-05 15:05:16 +0000

    The libmv_core library can now be created !
    I have made some needed changes so need interaction between us and libmv member to validate / invalidate modification I made.

--- trunk/otherProjects/libmv_core/tests/resection_test.cc
+++ trunk/otherProjects/libmv_core/tests/resection_test.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2007, 2008 libmv authors.
+// Copyright (c) 2009 libmv authors.
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to
@@ -17,7 +17,43 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
-//
-// Routines for solving polynomials.
 
-// TODO(keir): Add a solver for degree > 3 polynomials.
+#include <iostream>
+
+#include "../numeric/projection.h"
+#include "../estimator/projection_estimator.h"
+#include "testing.h"
+#include "test_data_sets.h"
+
+namespace {
+
+  using namespace libmv;
+  using namespace libmv::projection_estimation;
+
+  TEST(Resection, ThreeViews) {
+    int nviews = 5;
+    int npoints = 6;
+    NViewDataSet d = NRealisticCamerasFull(nviews, npoints);
+    for (int i = 0; i < nviews; ++i) {
+      Mat4X X(4, npoints);
+      X.block(0, 0, 3, npoints) = d.X;
+      X.row(3).setOnes();
+      const Mat2X &x = d.x[i];
+      Mat34 P;
+      Resection(x, X, &P);
+      Mat34 P_expected = d.P(i);
+
+      // Because the P matrices are homogeneous, it is necessary to be tricky
+      // about the scale factor to make them match.
+      P_expected *= 1/P_expected.array().abs().sum();
+      P *= 1/P.array().abs().sum();
+      if (!((P(0, 0) > 0 && P_expected(0, 0) > 0) ||
+        (P(0, 0) < 0 && P_expected(0, 0) < 0))) {
+          P *= -1;
+      }
+
+      EXPECT_MATRIX_NEAR(P_expected, P, 1e-9);
+    }
+  }
+
+} // namespace
